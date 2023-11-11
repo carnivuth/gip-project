e2f232e52072246bf2d4f7802fa076b593068eb7
carnivuth <matti200042@gmail.com>
2023-11-11 15:59:56 +0100

    added sleep

--- scraping.sh
+++ scraping.sh
@@ -8,6 +8,7 @@ for index in {1..99} ; do
         echo "curl $i index"
         curl -s -H  'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/114.0'\
     "$BASE_URL/ateco/$ATECO_CODE?page=$index&ordering=-ultimo_fatturato" > "aziende/index/tmp-$index-$ATECO_CODE-origin.html"
+        sleep 5
          cat "aziende/index/tmp-$index-$ATECO_CODE-origin.html"   | grep '<b class="dark-blue">' | cut -d '>' -f 2  | cut -d'<' -f 1 | awk '{print tolower($0)}'| sed 's/\ /-/g' > aziende/index/tmp-$index-$ATECO_CODE.html
     fi
     cat aziende/index/tmp-$index-$ATECO_CODE.html | while read azienda ; do
@@ -17,6 +18,6 @@ for index in {1..99} ; do
        -H  'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/114.0'\
     "$BASE_URL/azienda/$azienda"> aziende/"$azienda".html
         fi
-
+      sleep 5
     done 
 done
