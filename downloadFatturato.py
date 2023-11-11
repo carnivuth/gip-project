e7fc90bb57b1db122d1672f933cc09ac8332cfa7
LucaLombardi <87480145+Lombax99@users.noreply.github.com>
2023-11-11 16:28:24 +0100

    pushando il pytone

---
+++ downloadFatturato.py
@@ -0,0 +1,37 @@
+from time import sleep
+import requests
+import os
+
+#to do 1 -> 46
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/114.0'
+}
+
+url = "https://registroaziende.it/ateco/27.11?page="
+ordering = "&ordering=-ultimo_fatturato"
+
+subdir = "Cartella"
+baseFilename = "FatturatoPagina"
+
+#print(url+str(7)+ordering)
+
+
+for i in range(3, 47):
+    response = requests.get(url+str(i)+ordering, headers=headers)
+    print(response)
+
+    if b"Abbiamo notato" in response.content:
+        print("we done being goofed")
+
+    filename = baseFilename+str(i)+".html"
+    path = os.path.join(subdir, filename)
+
+    with open(path, mode='wb') as localfile:     
+        localfile.write(response.content)
+
+    sleep(5)
+
+
+
+
